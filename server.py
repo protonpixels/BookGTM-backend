1a148d693c3c82da1491215634ab9c46da615998
protonpixels <primelaunch34v@proton.me>
2026-06-21 12:26:34 -0400

    cors

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Book Downloads Predictor", version="1.0")
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://bookgtm.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
